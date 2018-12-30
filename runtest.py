bd1f6022983a38bde4d0cab36fb69eab6a43a6e6
chen ping <chenping@jenkins.com>
2018-12-30 11:05:41 +0800

    jenkins test main script runtest.py

---
+++ runtest.py
@@ -0,0 +1,31 @@
+import time
+import os
+
+import suit
+import HTMLTestRunnerCN
+
+if __name__ == '__main__':
+    # 添加Suite
+    print("run start....")
+    suiteTest=suit.suite()
+
+    #确定生成报告的路径
+    time_str=time.strftime("%Y-%m-%d-%H%M%S", time.localtime())
+    file_path = os.path.abspath(os.path.dirname(__file__)) + "\\report\\"
+    filename ="wap_" + time_str + ".html"
+    filefullpath=file_path+filename
+
+    fp = open(filefullpath,'wb')
+    #生成报告的Title,描述
+    runner = HTMLTestRunnerCN.HTMLTestRunner(
+        stream=fp,
+        title=u'wap自动化测试报告',
+        #description='详细测试用例结果',
+        # tester=u"DKDtester"
+        )
+    #运行测试用例
+
+    runner.run(suiteTest)
+    # 关闭文件，否则会无法生成文件
+    fp.close()
+    print("run complete!")
